81a68a96c5eed2038d4ae85d7ec590d75030cf94
Byancaam <byaam@hotmail.com>
2023-10-03 01:22:23 -0300

    feat: Resolucao dos exercicios de funcoes

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -3,10 +3,38 @@
 // As instruções estao em src/objects/exercises.txt
 // Escreva suas soluções aqui
 
-console.log("Boa sorte com os objetos!");
+console.log('Boa sorte com os objetos!');
 
 // codigo exemplo:
 
-const obj = { a: "a", b: "b", c: "c" };
+const obj = { a: 'a', b: 'b', c: 'c' };
 
 console.log(obj);
+
+/**
+ * Exercício 1 - Criação de Objeto
+ */
+
+interface Pessoa {
+  nome: string;
+  idade: number;
+  cidade: string;
+  profissao?: string;
+}
+
+const pessoa: Pessoa = {
+  nome: 'Maria Clara',
+  idade: 15,
+  cidade: 'Água Boa',
+};
+
+/**
+ * Exercício 2 - Acessando propriedades
+ */
+
+console.log('2. Idade:', pessoa.idade);
+
+/**
+ * Exercício 3 - Adicionando propriedades
+ */
+pessoa.profissao = 'programador';
